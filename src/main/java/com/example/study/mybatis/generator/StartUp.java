9baa6b1f2cad1ca059b0dfc91364397b3a93046c
caoyangfan <caoyangfan@meicai.cn>
2019-11-22 19:32:32 +0800

    mbg

---
+++ src/main/java/com/example/study/mybatis/generator/StartUp.java
@@ -0,0 +1,49 @@
+package com.example.study.mybatis.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 启动类
+ * 描述：补充 mybatis-generator 插件不能生成实体属性注释的功能
+ * from：https://www.cnblogs.com/NieXiaoHui/p/6094144.html
+ * title：Mybatis Generator的model生成中文注释
+ * @Author: chairuan
+ * @Date: 2018/3/19
+ */
+public class StartUp {
+    public static void main(String[] args) throws URISyntaxException {
+        try {
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream is = classloader.getResourceAsStream("generatorConfig.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+    }
+}
